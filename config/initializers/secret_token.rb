38b2da5d840896fed74b9489bbc1f4d44cd7d1fe
Matt <matthew.hinger@gmail.com>
2013-11-11 15:46:39 -0500

    Created rails application and added task create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskBlaster::Application.config.secret_token = 'ad736fcee34d40999ffb3e3dbf552fc8b5b2c710d221ba3b9a859805ceb5c7a9686d16b8ea326244670295e5e83bec1ad82300c8ded3076925a86995631cd02e'
